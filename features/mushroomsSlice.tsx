4cb48bbf9a41df8b2dae3bed285c00ec99073da6
DougMoraes <dougmoraes91@gmail.com>
2024-09-04 12:51:09 +0200

    feat: create MushroomsStore

---
+++ features/mushroomsSlice.tsx
@@ -0,0 +1,45 @@
+import getMushrooms from '@/api/api';
+import { Mushroom } from '@/types';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export interface MushroomsState {
+  points: Mushroom[],
+  statusGetMushrooms: 'ready' | 'loading' | 'failed',
+}
+
+const getMushroomsWrapper = async () => {
+  return await getMushrooms();
+};
+
+const initialState: MushroomsState = {
+  points: [],
+  statusGetMushrooms: 'loading',
+};
+
+export const fetchMushrooms = createAsyncThunk(
+  'mushrooms/getMushrooms',
+  async () => {
+    return await getMushrooms();
+  }
+);
+
+export const mushroomsSlice = createSlice({
+  name: 'mushrooms',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchMushrooms.pending, state => {
+      state.statusGetMushrooms = 'loading';
+    });
+    builder.addCase(fetchMushrooms.fulfilled, (state, action) => {
+      state.statusGetMushrooms = 'ready';
+      state.points = action.payload;
+    });
+    builder.addCase(fetchMushrooms.rejected, state => {
+      state.statusGetMushrooms = 'failed';
+      state.points = [];
+    });
+  },
+});
+
+export default mushroomsSlice.reducer;
---
+++ stores/MushroomsStore.tsx
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import mushroomsReducer from '../features/mushroomsSlice';
+
+export const MushroomsStore = configureStore({
+  reducer: {
+    mushrooms: mushroomsReducer,
+  },
+});
